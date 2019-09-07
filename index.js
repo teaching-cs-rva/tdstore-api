25a2db7b2f36e9806d863484c460832488e2b6a5
unknown <gerritbond@gmail.com>
2019-09-07 15:31:45 -0400

    Adding deal implementation, cleaning up import of data.

--- index.js
+++ index.js
@@ -5,19 +5,39 @@ const fs = require('fs');
 const app = express()
 const port = 3000
 
+// Load data on startup
+const shirts = JSON.parse(fs.readFileSync('./data/sample-shirts.json'));
+const deals = JSON.parse(fs.readFileSync('./data/sample-deals.json'));
+
 app.use(bodyParser.json());
 
 app.listen(port, () => console.log(`Example app listenting on port ${port}`));
 
 app.route('/deals')
-    .get((req, res, next) => (res.send(fs.readFileSync('./data/sample-deals.json'))));
+    .get((req, res, next) => (res.send(deals)));
+
+app.route('/deal/:dealid')
+    .get((req, res, next) => {
+        // Parse the dealid from the url
+        let dealid = req.params.dealid;
+
+        // Fetch the deal from the loaded array of deals
+        let deal = deals.filter(d => {
+            return d.id == dealid;
+        });
+
+        // If we don't have a deal, return a 404
+        if (deal === undefined || deal == null || deal.length == 0)
+            res.sendStatus("404");
+        
+        // Send the resulting deal
+        res.send(deal[0]);
+    });
 
 app.route('/shirts')
-    .get((req, res, next) => (res.send(fs.readFileSync('./data/sample-shirts.json'))));
+    .get((req, res, next) => (res.send(shirts)));
 
 app.route('/shirt')
     .get((req, res, next) => {res.send("Hello world")})
     .post((req, res, next) => (res.send(req.body)));
 
-app.route('deal')
-    .get((req, res, next) => (res.send("This is a deal")));
